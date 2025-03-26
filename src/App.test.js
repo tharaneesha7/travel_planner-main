292fe0bd3db7e59afd192ac397a49aeca022d577
tharaneesh <tharaneesha.22it@kongu.edu>
2025-03-26 11:23:04 +0530

    No code changes made

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders AC button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/AC/i);
+  expect(buttonElement).toBeInTheDocument();
 });
